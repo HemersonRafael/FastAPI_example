c7127d82bb56abeb49da5b77268b8fe101467bd3
HemersonRafael <rafaelpontes1995@gmail.com>
2021-08-17 21:21:29 -0300

    Adding update for author and book.

--- app/crud.py
+++ app/crud.py
@@ -4,23 +4,6 @@ from sqlalchemy.orm import Session
 from app import models, schemas
 
 
-def get_book(db: Session, book_id: int):
-    return db.query(models.Book).filter(models.Book.id == book_id).first()
-
-def get_book_by_title(db: Session, title: str):
-    return db.query(models.Book).filter(models.Book.title == title).first()
-
-def get_books(db: Session, skip: int = 0, limit: int =100):
-    return db.query(models.Book).offset(skip).limit(limit).all()
-
-def create_book(db: Session, book: schemas.BookCreate):
-    db_book = models.Book(title=book.title, description=book.description)
-    db.add(db_book)
-    db.commit()
-    db.refresh(db_book)
-    return db_book
-
-
 def get_author(db: Session, author_id: int):
     return db.query(models.Author).filter(models.Author.id == author_id).first()
 
@@ -43,8 +26,39 @@ def delete_author(db: Session, author_id: int):
     db.commit()
     return db_author
 
+def update_author(db: Session, db_author: schemas.AuthorInDB, author: schemas.AuthorUpdate):
+    db_author.name = author.name
+    db_author.books = author.books  
+    db.commit()
+    db.refresh(db_author)
+    return db_author
+
+
+def get_book(db: Session, book_id: int):
+    return db.query(models.Book).filter(models.Book.id == book_id).first()
+
+def get_book_by_title(db: Session, title: str):
+    return db.query(models.Book).filter(models.Book.title == title).first()
+
+def get_books(db: Session, skip: int = 0, limit: int =100):
+    return db.query(models.Book).offset(skip).limit(limit).all()
+
+def create_book(db: Session, book: schemas.BookCreate):
+    db_book = models.Book(title=book.title, description=book.description)
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
 def delete_book(db: Session, book_id: int):
     db_book = db.query(models.Book).filter_by(id=book_id).first()
     db.delete(db_book)
     db.commit()
+    return db_book
+
+def update_book(db: Session, db_book: schemas.BookInDB, book: schemas.BookUpdate):
+    db_book.title = book.title
+    db_book.description = book.description 
+    db.commit()
+    db.refresh(db_book)
     return db_book
