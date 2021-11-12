7ac54cf9ece5d1be61d59a414426914b31619787
HemersonRafael <rafaelpontes1995@gmail.com>
2021-11-12 11:37:51 -0300

    Adjusting tests from book CRUD.

--- app/tests/crud/test_book.py
+++ app/tests/crud/test_book.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm  import Session
+from fastapi.encoders import jsonable_encoder
 
 from app import crud
 from app.schemas.book import BookCreate, BookUpdate
@@ -16,37 +17,40 @@ def test_create_book(db: Session) -> None:
     assert book.description == description
 
 def test_update_book(db: Session) -> None:
-    title = random_lower_string(num=20)
-    description = random_lower_string(num=20)
-    book_in = BookUpdate(title=title, description=description)
-    db_book = create_random_book(db=db)
-    book = crud.book.update(db=db, db_obj=db_book, obj_in=book_in)
-    assert book.title == title
-    assert book.description == description
-
+    title = random_lower_string()
+    description = random_lower_string()
+    book_in = BookCreate(title=title, description=description)
+    book1 = crud.book.create(db=db, obj_in=book_in)
+    description2 = random_lower_string()
+    book_update = BookUpdate(title=title, description=description2)
+    book2 = crud.book.update(db=db, db_obj=book1, obj_in=book_update) 
+    assert book1.id == book2.id
+    assert book1.title == book2.title
+    assert book2.description == description2
+    assert book2.description != description
 
 def test_delete_book(db: Session) -> None:
     book = create_random_book(db=db)
-    book_rm = crud.book.remove(db=db, id=book.id)
-    check = crud.book.get(db=db, id=book_rm.id)
-    assert  check == None
-    assert  book_rm.title == book.title
-    assert  book_rm.description == book.description
-    assert  book_rm.id == book.id
+    rm_book = crud.book.remove(db=db, id=book.id)
+    stored_book = crud.book.get(db=db, id=rm_book.id)
+    assert  stored_book is None
+    assert  rm_book.title == book.title
+    assert  rm_book.description == book.description
+    assert  rm_book.id == book.id
 
 def test_get_book_by_id(db: Session) -> None:
     book = create_random_book(db=db)
-    book_out = crud.book.get(db=db, id=book.id)
-    assert book_out.id == book.id
+    stored_book = crud.book.get(db=db, id=book.id)
+    assert stored_book.id == book.id
 
 def test_get_multi_book(db: Session) -> None:
     num = random_int_number()
     for x in range(num):
-        book_in = create_random_book(db=db)
-    db_books = crud.book.get_multi(db=db)
-    assert len(db_books) >= num
+        create_random_book(db=db)
+    stored_books = crud.book.get_multi(db=db)
+    assert len(stored_books) >= num
 
 def test_get_by_title(db: Session) -> None:
     book_in= create_random_book(db=db)
-    db_books = crud.book.get_multi(db=db,title=book_in.title)
-    assert 1 == len(db_books)
+    stored_books = crud.book.get_multi(db=db,title=book_in.title)
+    assert 1 == len(stored_books)
